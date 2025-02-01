a20b837de50d60830f0b09d2e9c1fe71bd72a0b4
artemisiaporto <artemisiaporto@outlook.com>
2025-02-01 09:35:57 -0300

    W02 Chamber Directory Page Initiated

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,25 @@
+//display last modification
+const today = new Date();
+
+document.getElementById("currentyear").textContent = today.getFullYear();
+
+const lastModif = new Date();
+
+document.getElementById("lastModified").textContent = `Last Modification: ${lastModif.toLocaleString('en-US', {
+    month: '2-digit',
+    day: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false
+})}`;
+
+//hamburguer button
+const hamButton = document.querySelector('#menu');
+const navElement = document.querySelector("#animateme");
+
+hamButton.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamButton.classList.toggle('open');
+});
